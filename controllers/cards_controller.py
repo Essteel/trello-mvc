18e9243b01ea4bc75b388ed1385633276799e140
Essteel <siannon.steel@gmail.com>
2022-10-20 13:47:49 +1030

    added cards model, controller, blueprint

---
+++ controllers/cards_controller.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+cards_bp = Blueprint('cards', __name__, url_prefix = '/cards')
+
+@cards_bp.route('/')
+# @jwt_required()
+def all_cards():
+    # user_id = get_jwt_identity() # get user from token
+    # stmt = db.select(User).filter_by(id=user_id) # query to db to get user with that id
+    # user = db.session.scalar(stmt) # set user
+    # if not authorize():
+    #     return {'error' : 'You must be an admin'}, 401
+    # select * from cards
+    # stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+    # cards = db.session.scalars(stmt)
+    # return CardSchema(many=True).dump(cards)
+    return 'all_cards route'
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from controllers.cards_controller import cards_bp
 import os
+
+db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-    db = SQLAlchemy(app)
+    db.init_app(app)
+    ma.init_app(app)
 
-    # @app.route('/')
-    # def index():
-    #     return 'Hello World'
+    app.register_blueprint(cards_bp)
 
     return app
