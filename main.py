bba662aafdff3b0aed1b26678f3c762903cc08b8
Essteel <siannon.steel@gmail.com>
2022-10-20 12:07:54 +1030

    Added database connection

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, world'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    db = SQLAlchemy(app)
+
+    # @app.route('/')
+    # def index():
+    #     return 'Hello World'
+
+    return app
