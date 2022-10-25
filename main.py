ee80d667aa6c360a0718d4618fca48d35954e9f6
Essteel <siannon.steel@gmail.com>
2022-10-25 10:52:22 +1030

    added error handler for individual cards and not_found

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import os
 def create_app():
     app = Flask(__name__)
 
+    @app.errorhandler(404)
+    def not_found():
+        return {'error': 'Not Found'}, 404
+
     app.config['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
