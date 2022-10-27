db0e5ec5c137bcce87599422105fcdd1660804ce
Essteel <siannon.steel@gmail.com>
2022-10-27 14:20:00 +1030

    Added validation

--- models/card.py
+++ models/card.py
@@ -1,6 +1,10 @@
 from init import db, ma
-from marshmallow import fields
-from marshmallow.validate import Length
+from marshmallow import fields, validates
+from marshmallow.validate import Length, OneOf, And, Regexp
+from marshmallow.exceptions import ValidationError
+
+VALID_PRIORITIES = ('Urgent', 'High', 'Low', 'Medium')
+VALID_STATUSES = ('To Do', 'Done', 'Ongoing', 'Testing', 'Deployed')
 
 class Card(db.Model):
     __tablename__ = 'cards'
@@ -9,7 +13,7 @@ class Card(db.Model):
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
     date = db.Column(db.Date) # Date created
-    status = db.Column(db.String)
+    status = db.Column(db.String, default=VALID_STATUSES[0])
     priority = db.Column(db.String)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
@@ -20,7 +24,21 @@ class Card(db.Model):
 class CardSchema(ma.Schema):
     user = fields.Nested('UserSchema', only=['name', 'email'])
     comments = fields.List(fields.Nested('CommentSchema', exclude=['card']))
-    title = fields.String(required=True, validate=Length(min=2))
+    title = fields.String(required=True, validate=And(
+        Length(min=2, error='Title must be at least 2 characters long.'),
+        Regexp('^[a-zA-Z0-9 ]+$', error='Only letters, numbers and spaces are valid.')
+        ))
+    status = fields.String(load_default=VALID_STATUSES[0], validate=OneOf(VALID_STATUSES))
+    priority = fields.String(required=True, validate=OneOf(VALID_PRIORITIES))
+    @validates('status')
+    def validate_status(self, value):
+        # If trying to set this card to 'Ongoing'
+        if value == VALID_STATUSES[2]:
+            stmt = db.select(db.func.count()).select_from(Card).filter_by(status=VALID_STATUSES[2])
+            count = db.session.scalar(stmt)
+            # If there is already a card with this status
+            if count > 0:
+                raise ValidationError('You can only have one card with the status "Ongoing" at a time.')
     class Meta:
         fields = ('id', 'title', 'description', 'date', 'status', 'priority', 'user', 'comments')
         ordered = True
